3467984d10cc8960dd6998c834797d94ca59975b
Collin Jackson <jackson@google.com>
2017-03-02 10:38:48 -0800

    mock image picker

---
+++ lib/image_picker.dart
@@ -0,0 +1,74 @@
+// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library image_picker;
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+abstract class ImagePicker {
+  /// ImageProviders appear on this stream when images are picked.
+  Stream<ImageProvider> get onImagePicked;
+
+  /// Initiate the process of picking an image.
+  void pickImage();
+}
+
+class PlatformImagePicker implements ImagePicker {
+  PlatformImagePicker._();
+
+  /// Singleton ImagePicker for the operating system.
+  ///
+  /// Currently our implementation does not support multiple platform image pickers.
+  static PlatformImagePicker instance = new PlatformImagePicker._();
+
+  static const PlatformMethodChannel channel = const PlatformMethodChannel('image_picker');
+
+  final StreamController<ImageProvider> _controller = new StreamController<ImageProvider>(
+    onListen: () {
+      PlatformMessages.setBinaryMessageHandler(channel, _handleImagePicked);
+    },
+    onCancel: () {
+      PlatformMessages.setBinaryMessageHandler(channel, null);
+    }
+  );
+
+  Stream<ImageProvider> get onImagePicked {
+    if (_controller == null) {
+      _controller = new StreamController<ImageProvider>(
+        onListen: () {
+          PlatformMessages.setBinaryMessageHandler(channel, _handleImagePicked);
+        },
+        onCancel: () {
+          PlatformMessages.setBinaryMessageHandler(channel, null);
+        }
+      );
+    }
+    return _controller.stream;
+  }
+
+  void _handleImagePicked() {
+    // TODO(jackson): Implement
+    print("_handleImagePicked called");
+  }
+
+  void pickImage() => PlatformMessages.invokeMethod(channel, 'pickImage');
+}
+
+/// For testing, a mock image picker that provides fake NetworkImage providers
+class MockImagePicker implements ImagePicker {
+  MockImagePicker({ this.url });
+
+  String url;
+
+  final StreamController<ImageProvider> _controller = new StreamController<ImageProvider>();
+
+  Stream<ImageProvider> get onImagePicked => _controller.stream;
+
+  void pickImage() {
+    ImageProvider provider = new NetworkImage(url);
+    _controller.add(provider);
+  }
+}
