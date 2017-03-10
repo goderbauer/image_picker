08c9900b4438f1af7bb3a9ffe587d9e780abc7a4
Collin Jackson <jackson@google.com>
2017-03-10 13:10:53 -0800

    not building yet, but progress maybe

--- android/src/main/java/io/flutter/plugin/ImagePickerPlugin.java
+++ android/src/main/java/io/flutter/plugin/ImagePickerPlugin.java
@@ -1,25 +1,44 @@
 package io.flutter.plugin;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import io.flutter.app.FlutterActivity;
 import io.flutter.view.FlutterView;
-import org.chromium.base.Log;
+import io.flutter.plugin.common.FlutterMethodChannel;
+import io.flutter.plugin.common.FlutterMethodChannel.MethodCall;
+import io.flutter.plugin.common.FlutterMethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.FlutterMethodChannel.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.esafirm.imagepicker.features.ImagePicker;
+import com.esafirm.imagepicker.features.ImagePickerActivity;
+import com.esafirm.imagepicker.features.camera.CameraModule;
+import com.esafirm.imagepicker.features.camera.ImmediateCameraModule;
+import com.esafirm.imagepicker.features.camera.OnImageReadyListener;
+import com.esafirm.imagepicker.model.Image;
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Log;
 
 /**
  * Location Plugin
  */
-public class ImagePickerPlugin implements FlutterView.OnMessageListener {
-  private static String TAG = "ImagePickerPlugin";
+public class ImagePickerPlugin implements MethodCallHandler {
+  private static String TAG = "flutter";
+  private static final String CHANNEL = "ImagePicker";
 
-  public static final int IMAGE_PICKER_REQUEST_CODE = 2342;
+  public static final int REQUEST_CODE_PICK = 2342;
+  public static final int REQUEST_CODE_CAMERA = 2343;
 
   private FlutterActivity activity;
-  private ImagePickerPlugin plugin;
+  private static ImagePickerPlugin plugin;
+
+  // Pending method call to obtain an image
+  private Response pendingResponse;
 
   public static void register(FlutterActivity activity) {
     plugin = new ImagePickerPlugin(activity);
@@ -27,15 +46,67 @@ public class ImagePickerPlugin implements FlutterView.OnMessageListener {
 
   private ImagePickerPlugin(FlutterActivity activity) {
     this.activity = activity;
-    activity.getFlutterView().addOnMessageListener("ImagePicker", this);
+    new FlutterMethodChannel(activity.getFlutterView(), CHANNEL).setMethodCallHandler(this);
   }
 
   @Override
+  public void onMethodCall(MethodCall call, Response response) {
+    if (pendingResponse != null) {
+      response.error("ALREADY_ACTIVE", "Image picker is already active", null);
+    }
+    pendingResponse = response;
+    if (call.method.equals("pick")) {
+      ImagePicker.create(activity)
+          .single()
+          .start(REQUEST_CODE_PICK);
+    } else if (call.method.equals("capture")) {
+      DefaultCameraModule cameraModule = new DefaultCameraModule();
+      startActivityForResult(cameraModule.getIntent(context), RC_REQUEST_CAMERA);
+    } else {
+      throw new IllegalArgumentException("Unknown method " + call.method);
+    }
+  }
+
   public static void onActivityResult(Context context, int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_CODE_PICK) {
+      if (resultCode == Activity.RESULT_OK && data != null) {
+        ArrayList<Image> images = (ArrayList<Image>) ImagePicker.getImages(data);
+        Log.v(TAG, "We got images picked: " + images.size());
+        printImages(images);
+        plugin.handleResponse(images.get(0));
+      } else {
+        result.error("PICK_ERROR", "Error picking image", null);
+        plugin.handleResponse(null);
+      }
+    } else if (requestCode == REQUEST_CODE_CAMERA) {
+      if (resultCode == Activity.RESULT_OK && data != null)
+        cameraModule.getImage(context, data, new OnImageReadyListener() {
+          @Override
+          public void onImageReady(List<Image> images) {
+            Log.v(TAG, "We got images from the camera");
+            printImages(images);
+            //plugin.handleResponse(images);
+          }
+        });
+    }
   }
 
-  @Override
-  public String onMessage(FlutterView flutterView, String message) {
-    return null;
+  private void handleResponse(Image image) {
+    if (pendingResponse != null) {
+      pendingResponse.success(null);
+      pendingResponse = null;
+    } else {
+      throw new IllegalStateException("Received images from picker that were not requested");
+    }
+  }
+
+  private static void printImages(List<Image> images) {
+    if (images == null) return;
+
+    StringBuilder stringBuffer = new StringBuilder();
+    for (int i = 0, l = images.size(); i < l; i++) {
+      stringBuffer.append(images.get(i).getPath()).append("\n");
+    }
+    Log.v(TAG, stringBuffer.toString());
   }
 }
--- example/android/app/src/main/java/com/yourcompany/app/MainActivity.java
+++ example/android/app/src/main/java/com/yourcompany/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yourcompany.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.ImagePickerPlugin;
